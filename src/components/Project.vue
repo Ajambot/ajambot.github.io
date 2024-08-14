502eea403a2adbb673ecc23b28788b7e3585c441
Martin Morales <mmorale5@lakeheadu.ca>
2024-08-14 16:24:34 -0400

    Make technologies used list for projects component horizontally scrollable in small screens

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -11,36 +11,35 @@ interface Props {
 const { title, bulletpoint, techStack, image } = defineProps<Props>();
 const techStackPaths = computed(() => {
     let newPaths: Record<string, string | undefined> = {};
-    for(const key in techStack){
-        const newKey = "/src/assets/images/logos/"+key;
+    for (const key in techStack) {
+        const newKey = "/src/assets/images/logos/" + key;
         newPaths[newKey] = techStack[key];
     }
     return newPaths;
 });
-const imagePath = computed(() => "/src/assets/images/projects/"+image);
+const imagePath = computed(() => "/src/assets/images/projects/" + image);
 </script>
 <template>
     <div class="flex flex-col xl:w-2/5 border-black border-2 mx-auto rounded-2xl bg-gray-50">
         <div class="flex flex-wrap p-5 items-center gap-5">
-            <h2  class="w-4/5">{{ title }}</h2>
+            <h2 class="w-4/5">{{ title }}</h2>
             <div class="flex items-center gap-3">
                 <a :href="github" class="basis-full">
-                    <img class="aspect-square w-16" src="/src/assets/images/logos/Github.png" alt="Github Icon"/>
+                    <img class="aspect-square w-16" src="/src/assets/images/logos/Github.png" alt="Github Icon" />
                 </a>
                 <a :href="link" class="basis-full">
-                    <img class="aspect-square w-16" src="/src/assets/images/logos/Webpage Icon.png" alt="Icon representing a webpage">
+                    <img class="aspect-square w-16" src="/src/assets/images/logos/Webpage Icon.png"
+                        alt="Icon representing a webpage">
                 </a>
             </div>
         </div>
-        <div class="p-5 ">
-            <ul class="flex overflow-x-scroll gap-10 w-fit p-3 rounded-md">
-                <li v-for="(link, img) in techStackPaths">
-                    <a :href="link" target="_blank">
-                        <img class="drop-shadow-lg h-14 hover:scale-110 transition ease-in-out" :src="img">
-                    </a>
-                </li>
-            </ul>
-        </div>
+        <ul class="flex overflow-x-auto overflow-y-hidden w-fit py-8 mx-5 gap-5 rounded-md">
+            <li v-for="(link, img) in techStackPaths" class="flex-shrink-0 h-fit w-fit">
+                <a :href="link" target="_blank" class="h-14 block">
+                    <img class="drop-shadow-lg h-full object-contain hover:scale-110 transition ease-in-out" :src="img">
+                </a>
+            </li>
+        </ul>
         <ul class="p-5 space-y-4 list-inside leading-relaxed overflow-scroll h-1/3">
             <li class="list-disc text-xl" v-for="(item) in bulletpoint">{{ item }}</li>
         </ul>
