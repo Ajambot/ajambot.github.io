e027f765b8dc097f923faf742907b0da7aa5bdd6
Martin Morales Arana <mmorale5@lakeheadu.ca>
2026-04-23 18:59:10 -0400

    Add new projects lorcana engine and hackathon and make small updates

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="about" class="flex flex-wrap justify-center p-5 gap-7 sm:gap-16 sm:mt-16 scroll-mt-20">
-    <img class="min-w-64 sm:min-w-72 w-1/5 rounded-3xl my-auto" src="../assets/images/portrait.jpeg"
+    <img class="min-w-64 sm:min-w-72 w-1/5 rounded-3xl my-auto" src="../assets/images/portrait.jpg"
       alt="Portrait photo of Martin Morales" />
     <div class="my-auto w-4/5 sm:w-3/5">
       <h2>Hello, I'm Martin</h2>
       <p>
-        a Computer Science student at Lakehead University with a passion for
+        a Computer Science graduate from Lakehead University with a passion for
         data analysis, software development, and problem-solving. With a keen
         interest in creating innovative solutions and a strong academic record,
-        I am currently open for new grad Software Engineer opportunities starting January
-        2026. Explore my portfolio, and let's connect to discuss how we can
-        collaborate on impactful projects.
+        I am currently open for Data Analyst and Software Engineer opportunities. 
+        Explore my portfolio, and let's connect to discuss how we can collaborate on impactful projects.
       </p>
-      <a target="_blank" href="https://docs.google.com/document/d/1bWJbVQldPWEetv6TdtJ95dHFfcx28r8g">
+      <a target="_blank" href="https://docs.google.com/document/d/1Sz_HSkAtqzRtkAzlKx_SXWo_zckWJjV_d4sjOoDMKvQ">
         <button class="bg-black text-white p-2 my-2 hover:bg-gray-800 font-semibold rounded-lg" type="button">View
           Resume</button>
       </a>
