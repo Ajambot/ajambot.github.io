f62ffa78dc8be4ebb8f02689d8def1e98b174dc5
Martin Morales Arana <mmorale5@lakeheadu.ca>
2026-09-08 18:49:48 -0400

    Add new work experience, skills, change hero image, and reword intro parragraph

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,15 +1,11 @@
 <template>
   <div id="about" class="flex flex-wrap justify-center p-5 gap-7 sm:gap-16 sm:mt-16 scroll-mt-20">
-    <img class="min-w-64 sm:min-w-72 w-1/5 rounded-3xl my-auto" src="../assets/images/portrait.jpg"
+    <img class="min-w-64 sm:min-w-72 w-1/5 rounded-3xl my-auto" src="../assets/images/portrait.jpeg"
       alt="Portrait photo of Martin Morales" />
     <div class="my-auto w-4/5 sm:w-3/5">
       <h2>Hello, I'm Martin</h2>
       <p>
-        a Computer Science graduate from Lakehead University with a passion for
-        data analysis, software development, and problem-solving. With a keen
-        interest in creating innovative solutions and a strong academic record,
-        I am currently open for Data Analyst and Software Engineer opportunities. 
-        Explore my portfolio, and let's connect to discuss how we can collaborate on impactful projects.
+        I am a software developer driven by a passion for solving real-world, complex problems through abstraction and code. I approach challenges with the belief that most problems can be decomposed and addressed through thoughtful software design. I enjoy continuously building projects on GitHub and sharing my experience on my Youtube channel. I bring multiple perspectives to software design including experience across frontend and backend development, databases, AI, system design, and data analysis.
       </p>
       <a target="_blank" href="https://docs.google.com/document/d/1Sz_HSkAtqzRtkAzlKx_SXWo_zckWJjV_d4sjOoDMKvQ">
         <button class="bg-black text-white p-2 my-2 hover:bg-gray-800 font-semibold rounded-lg" type="button">View
@@ -25,6 +21,10 @@
     <a href="https://www.github.com/Ajambot" target="_blank">
       <img class="object-contain h-full hover:scale-110 transition ease-in-out" src="../assets/images/logos/Github.png"
         alt="Github Logo">
+    </a>
+    <a href="https://www.youtube.com/@AjambotTech?sub_confirmation=1" target="_blank">
+      <img class="object-contain h-full hover:scale-110 transition ease-in-out" src="../assets/images/logos/Youtube.webp"
+        alt="Youtube Logo">
     </a>
     <a href="mailto:mmorale5@lakeheadu.ca" target="_blank">
       <img class="h-full hover:scale-110 transition ease-in-out" src="../assets/images/logos/Email.png"
